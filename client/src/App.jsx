21d908fa88b103fa50b2e3db628a29e7efa475ae
firdaoues <firdaouesmiraoui@gmail.com>
2022-09-01 20:38:50 +0200

    client-side Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,17 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import {ChanellListContainer, ChanellContainer  } from './components';
+import {ChanellListContainer, ChanellContainer, Auth  } from './components';
 import './App.css';
 
 const apiKey = '9cqndebr4vzn';
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) return <Auth />
   return (
     <div className='app__wrapper'> 
        <Chat client={client} theme="team light">
