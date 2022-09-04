591e2bff361c51844e86c43a28b32a7313477c37
firdaoues <firdaouesmiraoui@gmail.com>
2022-09-04 13:12:33 +0200

    switchmode

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -8,6 +8,9 @@ const Auth = () => {
     const [isSignup, setIsSignup] = useState(true);
 
     const handleChange = () => {};
+    const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    };
   return (
     <div className="auth__form-container">
         <div className="auth__form-container_fields">
@@ -20,8 +23,53 @@ const Auth = () => {
                             <input name="fullName" type="text" placeholder="Full Name" onChange={handleChange} required />
                            
                         </div>
+                        
                     )}
+                     <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="username"> Username</label>
+                            <input name="username" type="text" placeholder="Username" onChange={handleChange} required />
+                           
+                        </div>
+                        {isSignup && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="phoneNumber"> Phone Number</label>
+                            <input name="phoneNumber" type="text" placeholder="Phone Number" onChange={handleChange} required />
+                           
+                        </div>
+                        
+                    )}
+                     {isSignup && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="AvatarURL"> Avatar URL</label>
+                            <input name="avatarURL" type="text" placeholder="Avatar URL" onChange={handleChange} required />
+                           
+                        </div>
+                        
+                    )}
+                     <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="password"> Password</label>
+                            <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+                           
+                        </div>
+                        {isSignup && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                            <input name="Confirmassword" type="password" placeholder="Confirm Password" onChange={handleChange} required />
+                           
+                        </div>
+                        )}
                 </form>
+                <div className='auth__form-container_fields-account'>
+                    <p> 
+                        {isSignup 
+                        ? "Already have an Account ?"
+                        : "Don't have an Account ?"
+                        }
+                        <span onClick={switchMode}>
+                            {isSignup ? "Sign In" :" Sign Up"}
+                        </span>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
